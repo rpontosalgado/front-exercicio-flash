7ee0281fc045f3605502de74e97e247489f54cba
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2021-02-13 12:16:32 -0300

    Adicionado funcionalidades da tela home page

--- src/screens/HomePage/HomePage.js
+++ src/screens/HomePage/HomePage.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { Button } from "antd";
+import { useRequestData } from "../../hooks/useRequestData";
+import { goToEmployeeList } from "../../routes/Coordinator";
 
 const HomePage = () => {
+
+  const history = useHistory();
+
+  const companiesData = useRequestData({}, "/company")
+  const companyNames = companiesData.companies;
+
+  const renderCompanyNamesButtonList = () => (
+    companyNames.map(item => (
+      <Button
+        type = "default"
+        onClick = {() => goToEmployeeList(history, item)}
+      >
+        { item }
+      </Button>
+    ))
+  );
 
   return (
     <main>
